09b7bf56d202008c73b6c66c8646b55b1db3413d
Samu06g <samuel.gonzaleztrueba@iesmiguelherrero.com>
2023-01-13 17:05:50 +0100

    Mensaje de bienvenida al programa

---
+++ Calculadora_SamuelGonzalez/src/main/java/calculadora/calculadora_samuelgonzalez/Calculadora_SamuelGonzalez.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package calculadora.calculadora_samuelgonzalez;
+
+/**
+ *
+ * @author DAM117
+ */
+public class Calculadora_SamuelGonzalez {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos al programa de la Calculadora");
+    }
+}
