837a8d3e6db782ef6d7d0fb4033f213f157b4888
Samu06g <samuel.gonzaleztrueba@iesmiguelherrero.com>
2023-01-13 18:06:35 +0100

    Inicializo repositorio desde Netbeans

--- Calculadora_SamuelGonzalez/src/main/java/calculadora/calculadora_samuelgonzalez/Calculadora_SamuelGonzalez.java
+++ Calculadora_SamuelGonzalez/src/main/java/calculadora/calculadora_samuelgonzalez/Calculadora_SamuelGonzalez.java
@@ -1,19 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package calculadora.calculadora_samuelgonzalez;
 
+import java.util.Scanner;
+
 /**
  *
- * @author DAM117
+ * @author AlvaroD
  */
 public class Calculadora_SamuelGonzalez {
+    
+    
+    static Scanner scanner = new Scanner(System.in);
+    static int opcion = -1; //opción del menú
+    static int numero1 = 0, numero2 = 0; //Variables de entrada
 
     public static void main(String[] args) {
-        System.out.println("Bienvenidos al programa de la Calculadora");
-        float numero1;
-        float numero2;
+        System.out.println("Bienvenidos al programa de la calculadora");
+        while (opcion != 0) {
+            //Try catch para evitar que el programa termine si hay un error
+            try {
+                System.out.println("Elige opción:\n" + ""
+                        + "1.- Sumar\n"
+                        + "2.- Restar\n"
+                        + "3.- Multiplicar\n"
+                        + "4.- Dividir\n"
+                        + "0.- Salir");
+
+                System.out.println("Selecciona una opción de 0 a 4");
+                opcion = Integer.parseInt(scanner.nextLine());
+
+                switch (opcion) {
+                    case 1 -> {
+                        pideNumeros();
+                        System.out.println(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+                    }
+                    case 2 -> {
+                        pideNumeros();
+                        System.out.println(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+                    }
+                    case 3 -> {
+                        pideNumeros();
+                        System.out.println(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+                    }
+                    case 4 -> {
+                        pideNumeros();
+                        System.out.println(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+                    }
+                    case 0 -> System.out.println("Saliendo...");
+                    default -> System.out.println("Opción no disponible");
+                }
+
+                System.out.println("\n");
+
+            } catch (NumberFormatException e) {
+                System.out.println("Error!");
+            }
+        }
+
+    }
+
+    //Método para recoger las variables de entrada
+    public static void pideNumeros() {
+        System.out.println("Introduce el primer número:");
+        numero1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Introduce el segundo número:");
+        numero2 = Integer.parseInt(scanner.nextLine());
+
     }
 }
